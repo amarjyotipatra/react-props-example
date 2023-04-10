baac25a9e42cd166e53f866d66a07390bf9212ac
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2023-04-10 12:49:03 +0530

    Added a Event

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./styles.css";
-import ChildValue from "./Child.js"
-import {useState} from "react"
+import ChildValue from "./Child.js";
+import { useState } from "react";
 
 export default function App(props) {
-  let [counter,setCounter]=useState(0)
+  let [counter, setCounter] = useState(0);
+  let callback = (val) => setCounter(val);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <button onClick={()=>setCounter(++counter)}>Add</button>
-      <ChildValue counterValue={counter} />
+      <button onClick={() => setCounter(++counter)}>Add</button>
+      <ChildValue callbackFunc={callback} counterValue={counter} />
     </div>
   );
 }
--- src/Child.js
+++ src/Child.js
@@ -1,3 +1,8 @@
 export default function ChildValue(props) {
-  return <div>{props.counterValue}</div>;
+  return (
+    <div>
+      <h1>{props.counterValue}</h1>
+      <button onClick={() => props.callbackFunc(25)}>Set Value To 25</button>
+    </div>
+  );
 }
